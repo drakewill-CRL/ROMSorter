744876e02b063be574171c8ec870fb051b3beb05
drakewill <drake@drakewill-crl>
2022-05-23 12:56:39 -0400

    Finally have a MAUI project template that compiles.

---
+++ RomSorterMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace RomSorterMaui
+{
+    [Register(nameof(AppDelegate))]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ RomSorterMaui/Resources/Colors.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RomSorterMaui
+{
+    public partial class Colors : ResourceDictionary
+    {
+        public Colors()
+        {
+            InitializeComponent();
+        }
+    }
+}
